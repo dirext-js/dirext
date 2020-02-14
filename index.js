5fc7d12557a6d59ae1a4bc0e71ddb43aed08de93
SARA POWERS <sarapowers029@gmail.com>
2020-02-14 12:16:08 -0500

    added route splitter functionality

--- index.js
+++ index.js
@@ -14,8 +14,32 @@ class Dirext {
     this.options = this.set.bind(this, 'OPTIONS');
     this.trace = this.set.bind(this, 'TRACE');
   }
+
   // create a helper function that takes each segment of route and creates an array of key value pairs
   // edge case for global middleware
+  static routeSplitter(url) {
+    // split route on each / then map through elements
+    const route = url.split('/').splice(1).map((elem) => {
+      // if element is a param return an object with key param value elem
+      if (elem[0] === ':') return { param: elem.slice(1) };
+      // if element is a query string
+      if (elem[0] === '?') {
+        // split on & to seperate multiple queries
+        const queryArr = elem.slice(1).split('&');
+        // reduce query arr to an object holding key query which holds key value pairs of queries
+        return queryArr.reduce((obj, query) => {
+          if (!obj.query) obj.query = {};
+          const split = query.split('=');
+          obj.query[split[0]] = split[1];
+          return obj;
+        }, {});
+      }
+      // else return obj with key route and value of elem
+      return { route: elem };
+    });
+    // return array of routes
+    return route;
+  }
 
   // method to add routes for router to recognicse
   set(method, url, ...middlewareFuncs) {
@@ -36,15 +60,8 @@ class Dirext {
     return this;
   }
 
-  // /ELI/izumi/kang  /get
-  // dirext.use('/eli', someMiddleware)
-
   // dirext.post('eli/)
   find(url, method) {
-    // helper function for slicing urls
-    const helperSlice = (url) => {
-
-    };
     // check if the two arrays are the same length, if they aren't, continue with loop
     // confirm that all route segment objects hold the same value
     // if there is a parameter in the url stored in routes, the value at that index doesn't matter
